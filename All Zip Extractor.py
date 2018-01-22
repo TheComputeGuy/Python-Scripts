62a65e359fe21f8d86b2837d54183fa21a0303fe
Shubham Pharande <23342972+Shubham-272@users.noreply.github.com>
2018-01-22 20:04:09 +0530

    Update All Zip Extractor.py

--- All Zip Extractor.py
+++ All Zip Extractor.py
@@ -1,16 +1,16 @@
 import os
 from zipfile import ZipFile
-path=raw_input('Enter path to the folder') #Folder which contains the zip files
+path=raw_input('Enter path to the folder: ')    #Folder which contains the zip files
 pathList=[]
 
 for i in os.listdir(path):
-    if i[len(i)-4:len(i)]=='.zip':          #If extension is .zip:
-        pathList.append(path+'\\'+i)        #then add filename+path to a new list
+    if i[len(i)-4:len(i)]=='.zip':              #If extension is .zip:
+        pathList.append(path+'\\'+i)            #then add filename+path to a new list
 
 for j in pathList:
-    with ZipFile(j, 'r') as zip:            #Open file
-        zip.extractall(path)                #Extract all contents to 'path'
-        zip.close()                         #Close zip file
+    with ZipFile(j, 'r') as zip:                #Open file
+        zip.extractall(path)                    #Extract all contents to 'path'
+        zip.close()                             #Close zip file
 
 print 'Done!'
 
