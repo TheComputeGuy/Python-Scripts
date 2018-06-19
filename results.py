747e299a223378f18dd17749ade6ecf757c13651
Shubham Pharande <shubhampharande8@gmail.com>
2018-06-20 01:02:59 +0530

    Added results.py

---
+++ results.py
@@ -0,0 +1,19 @@
+import requests
+import getpass
+
+username=input("Enter your roll number: ").strip()
+
+password=getpass.getpass("Enter your MIS password: ")
+
+url="https://delta.nitt.edu/results/results.php"
+
+payload={"uname":username, "pwd":password}
+
+headers={"Content-Type":"application/x-www-form-urlencoded"}
+
+session=requests.session()
+r=session.post(url, data=payload, headers=headers)
+
+output=r.text[6:len(r.text)-6]
+
+print(output)
